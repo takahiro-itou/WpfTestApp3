c6a8d28704020184e6d695a79125778054a72f58
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-05 16:16:25 +0900

    複数 ViewModel の練習

--- WpfTestApp3/App.xaml.cs
+++ WpfTestApp3/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿
 using System.Windows;
+using WpfTestApp3.Models;
+using WpfTestApp3.Services;
+using WpfTestApp3.ViewModels;
+using WpfTestApp3.Views;
 
 namespace WpfTestApp3
 {
@@ -9,6 +12,24 @@ namespace WpfTestApp3
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var storage = new JsonCounterStorage();
+
+            var model = new CounterModel();
+
+            var counterViewModel = new CounterViewModel(model, storage);
+            var evenOddViewModel = new EvenOddViewModel(model);
+
+            var mainWindow = new MainWindow();
+            mainWindow.CounterView.SetViewModel(counterViewModel);
+            mainWindow.EvenOddView.SetViewModel(evenOddViewModel);
+
+            mainWindow.Show();
+        }
+
     }
 
 }
