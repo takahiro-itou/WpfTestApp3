fa56fdbabde7db5509a4e584fab41df7d5797e02
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-05 15:47:08 +0900

    作業途中
    EvenOddView, EvenOddViewModel を作成

---
+++ WpfTestApp3/ViewModels/EvenOddViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfTestApp3.Models;
+
+namespace WpfTestApp3.ViewModels
+{
+    public class EvenOddViewModel : INotifyPropertyChanged
+    {
+        private readonly CounterModel _model;
+
+        public EvenOddViewModel(CounterModel model)
+        {
+            _model = model;
+            _model.ValueChanged += OnCountChanged;
+        }
+
+        public string EvenOddText => _model.Value % 2 == 0 ? "Even" : "Odd";
+
+        private void OnCountChanged()
+        {
+            OnPropertyChanged(nameof(EvenOddText));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
